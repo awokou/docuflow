7cf38c4ac5e887e7505ae040421dd387cfaef4cb
awokou <kawokou122@gmail.com>
2026-03-12 14:01:11 +0100

    Update project

--- api-docuflow/src/main/java/com/server/apidocuflow/service/CategoryService.java
+++ api-docuflow/src/main/java/com/server/apidocuflow/service/CategoryService.java
@@ -1,4 +1,13 @@
 package com.server.apidocuflow.service;
 
+import com.server.apidocuflow.domain.dto.external.CategoryDto;
+import com.server.apidocuflow.domain.dto.response.CategoryResponse;
+
+import java.util.List;
+
 public interface CategoryService {
+
+    CategoryDto saveCategory(CategoryDto categoryDto);
+
+    List<CategoryResponse> getAllCategory();
 }
--- api-docuflow/src/main/java/com/server/apidocuflow/service/impl/CategoryServiceImpl.java
+++ api-docuflow/src/main/java/com/server/apidocuflow/service/impl/CategoryServiceImpl.java
@@ -1,13 +1,33 @@
 package com.server.apidocuflow.service.impl;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.server.apidocuflow.domain.dto.external.CategoryDto;
+import com.server.apidocuflow.domain.dto.response.CategoryResponse;
+import com.server.apidocuflow.domain.mapper.CategoryMapper;
 import com.server.apidocuflow.repository.CategoryRepository;
 import com.server.apidocuflow.service.CategoryService;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class CategoryServiceImpl implements CategoryService {
 
     private final CategoryRepository categoryRepository;
+    private final CategoryMapper categoryMapper;
+
+    @Override
+    public CategoryDto saveCategory(CategoryDto categoryDto) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'saveCategory'");
+    }
+
+    @Override
+    public List<CategoryResponse> getAllCategory() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAllCategory'");
+    }
 }
